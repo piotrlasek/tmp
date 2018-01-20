ead84c2946c6572be795f9aa26ee71fb37d9dc64
piotrlasek <piotrlasek@users.noreply.github.com>
2018-01-20 11:12:11 +0100

    Update progc_04.c

--- progc_04.c
+++ progc_04.c
@@ -1,3 +1,34 @@
+// założenie: długość s1 == długość s2
+
+// wersja 1
+int compare(char *s1, char *s2) {
+	int result = 0;
+	int i = 0;
+
+	// s1[i] == *(s1 + i)
+	while (*(s1 + i) != '\0')
+	{
+		char c1 = *(s1 + i);
+		char c2 = *(s2 + i);
+
+		if (c1 > c2)
+		{
+			result = 1;
+			break;
+		}
+		else if (c2 > c1)
+		{
+			result = -1;
+			break;
+		}
+
+		i++;
+	}
+
+	return result;
+}
+
+// wersja 2
 int compare(char* s1, char* s2) {
 	int i = 0;
 	int result = 0;
